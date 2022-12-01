412a1c65049c1df3280569cafa5ebd7d70e30fbb
Tocmac <619689817@qq.com>
2022-12-01 22:39:55 +0800

    12.1 result

--- saved_logs/poc/finetune/nuimages/CMRC_2_1_new/cascade_mask_rcnn_r50_fpn_coco-20e_1x_nuim.py
+++ saved_logs/poc/finetune/nuimages/CMRC_2_1_new/cascade_mask_rcnn_r50_fpn_coco-20e_1x_nuim.py
@@ -241,8 +241,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=2,
+    samples_per_gpu=1,
+    workers_per_gpu=1,
     train=dict(
         type='CocoDataset',
         ann_file=
